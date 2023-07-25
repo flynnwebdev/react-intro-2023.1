5d208bbd789b24b8e67adc6ac1504a2ee2a964a0
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-07-25 10:42:21 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Greeting from "./Greeting"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Greeting />
+      <Greeting />
+      <p>Lorem ipsum dolor</p>
+      <Greeting />
+    </>
+  )
+}
+
+export default App
